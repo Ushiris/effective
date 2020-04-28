87df620e7d3ce7e3d54b7ee622b0c32185d9784d
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-04-28 16:14:48 +0900

    エフェクトオブジェクト当たり判定

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/EffectObjectHit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectObjectHit : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)// 当たった瞬間
+    {
+        Debug.Log("当たってるよ！");
+    }
+    private void OnTriggerExit(Collider other)// 離れた瞬間
+    {
+        Debug.Log("離れた！");
+    }
+}
