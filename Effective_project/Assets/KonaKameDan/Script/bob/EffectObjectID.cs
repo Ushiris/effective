4baf5d11ac05eb49730c693f13172b7f42a42ca8
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-04-28 17:29:18 +0900

    エフェクトオブジェクト取得

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/EffectObjectID.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EffectObjectID : MonoBehaviour
+{
+    public enum EffectObjectType
+    {
+        RED,
+        BLUE,
+        EYLLOW
+    }
+    public EffectObjectType effectObjectType;
+}
