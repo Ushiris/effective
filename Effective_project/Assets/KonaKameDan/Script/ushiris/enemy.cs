9cc02bccc17a941db0212d8a91fd7cdaf9c0fe5e
ushiris_home <ushiriscco@gmail.com>
2020-04-28 16:10:43 +0900

    add enemy

---
+++ Effective_project/Assets/KonaKameDan/Script/ushiris/enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    int HP = 1;
+
+    private void Awake()
+    {
+        if (HP <= 0)
+        {
+            Debug.Log("Default HP is 0.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HP <= 0)
+        {
+            Dead();
+        }
+    }
+
+    void Dead()
+    {
+        Destroy(gameObject);
+    }
+
+    int damege(int fouce)
+    {
+        HP -= fouce;
+        return fouce;
+    }
+}
