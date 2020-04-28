0fd55802d638a8df4a7e15fbce8801dc4b9dba74
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-04-28 15:38:33 +0900

    エフェクトオブジェクトぷかぷか

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/EffectObjectMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectObjectMove : MonoBehaviour
+{
+    public float rotationSpeed;
+    [SerializeField] float sin;// 上下運動用
+    void Update()
+    {
+        sin = Mathf.Sin(Time.time);
+        transform.Rotate(new Vector3(0.0f, rotationSpeed, 0.0f));
+        transform.position = new Vector3(0.0f, sin, 0.0f);
+    }
+}
