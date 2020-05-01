fceba8fc65c738af25031b2cf60110350d5b460c
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-05-01 14:10:37 +0900

    エフェクトオブジェクトを取得するスクリプト

--- Effective_project/Assets/KonaKameDan/Script/bob/EffectObjectAcquisition.cs
+++ Effective_project/Assets/KonaKameDan/Script/bob/EffectObjectAcquisition.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class EffectObjectAcquisition : MonoBehaviour
 {
+    [System.Serializable]
+    public class EffectObjectClass
+    {
+        [HideInInspector] public string name;
+        public int count;
+    }
     private EffectObjectID effectObjectID;
+    public List<EffectObjectClass> effectObjectAcquisition = new List<EffectObjectClass>();// リスト作成
+    public List<string> effectObjectName = new List<string>();// リスト作成
     private void OnTriggerEnter(Collider other)// 何かに当たった瞬間
     {
         GameObject anotherObject = other.gameObject;
@@ -13,15 +21,33 @@ public class EffectObjectAcquisition : MonoBehaviour
         switch (effectObjectID.effectObjectType)// 当たったエフェクトオブジェクトが何かを取得
         {
             case EffectObjectID.EffectObjectType.RED:
-                Debug.Log("赤");
+                if (effectObjectName.Contains("赤"))
+                    effectObjectAcquisition[effectObjectName.IndexOf("赤")].count++;// ストック追加
+                else
+                {
+                    effectObjectAcquisition.Add(new EffectObjectClass { name = "赤", count = 1 });// リストに作成
+                    effectObjectName.Add("赤");
+                }
                 break;
 
             case EffectObjectID.EffectObjectType.BLUE:
-                Debug.Log("青");
+                if (effectObjectName.Contains("青"))
+                    effectObjectAcquisition[effectObjectName.IndexOf("青")].count++;// ストック追加
+                else
+                {
+                    effectObjectAcquisition.Add(new EffectObjectClass { name = "青", count = 1 });// リストに作成
+                    effectObjectName.Add("青");
+                }
                 break;
 
             case EffectObjectID.EffectObjectType.EYLLOW:
-                Debug.Log("黄");
+                if (effectObjectName.Contains("黄"))
+                    effectObjectAcquisition[effectObjectName.IndexOf("黄")].count++;// ストック追加
+                else
+                {
+                    effectObjectAcquisition.Add(new EffectObjectClass { name = "黄", count = 1 });// リストに作成
+                    effectObjectName.Add("黄");
+                }
                 break;
 
             default:
