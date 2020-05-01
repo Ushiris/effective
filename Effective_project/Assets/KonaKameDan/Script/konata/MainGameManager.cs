8385b24632d58e28e09b76b2a391db98495f24be
konata016 <konata016@gmail.com>
2020-05-01 15:12:00 +0900

    MainGameManager追加
    MainGameManagerから所持しているエフェクトリストを持ってこれるぞ

---
+++ Effective_project/Assets/KonaKameDan/Script/konata/MainGameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// 今所持しているエフェクトリストを取得できるぞ！
+    /// </summary>
+    public static List<EffectObjectAcquisition.EffectObjectClass> GetPlEffectList
+    {
+        get { return new List<EffectObjectAcquisition.EffectObjectClass>(EffectObjectAcquisition.GetEffectObjAcquisition.effectObjectAcquisition); }
+    }
+}
