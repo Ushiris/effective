046c36ac4db0a1f6663a028bd7712ef788d255ac
konata016 <konata016@gmail.com>
2020-05-15 15:14:07 +0900

    画像差し替え

--- Effective_project/Assets/KonaKameDan/Script/konata/UI/UI_Image.cs
+++ Effective_project/Assets/KonaKameDan/Script/konata/UI/UI_Image.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class UI_Image : MonoBehaviour
 {
+    [NamedArrayAttribute(new string[]
+    {
+        "射撃","斬撃","防御","設置","拡散","追尾","吸収","爆発","遅延","飛翔"
+    })]
     public List<Sprite> effectIconList = new List<Sprite>();
 
     public static UI_Image GetUI_Image { get; private set; }
@@ -17,11 +22,35 @@ public class UI_Image : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+}
+
+public class NamedArrayAttribute : PropertyAttribute
+{
+    public readonly string[] names;
+    public NamedArrayAttribute(string[] names) { this.names = names; }
+}
+
+[CustomPropertyDrawer(typeof(NamedArrayAttribute))]
+public class NamedArrayDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+    {
+        try
+        {
+            int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+            EditorGUI.PropertyField(rect, property, new GUIContent(((NamedArrayAttribute)attribute).names[pos]));
+
+        }
+        catch
+        {
+            EditorGUI.PropertyField(rect, property, label);
+        }
     }
 }
