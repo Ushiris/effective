bce81d9a32d0acb462aa30f1c4ae895403b847b4
ushiris_home <ushiriscco@gmail.com>
2020-05-22 17:24:26 +0900

    fix life

--- Effective_project/Assets/KonaKameDan/Script/ushiris/Life.cs
+++ Effective_project/Assets/KonaKameDan/Script/ushiris/Life.cs
@@ -9,38 +9,29 @@ public class Life : MonoBehaviour
     public delegate void Dead();
     public delegate int DamageEvent(int true_damage);
 
-    public int MaxHP { get; private set; }
-    public int HP { get; private set; }
-    public uint BeatTime { get; private set; }
+    public int MaxHP { get; private set; } = int.MaxValue;
+    public int HP { get; private set; } = int.MaxValue;
+    public uint BeatTime { get; private set; } = 1;
+    public bool isFreeze { get; private set; } = false;
 
     StopWatch timer;
-    List<HeartBeat> beat=new List<HeartBeat>();
+    List<HeartBeat> beat = new List<HeartBeat>();
     List<Dead> dead = new List<Dead>();
     List<DamageEvent> damageEvent = new List<DamageEvent>();
     List<DamageEvent> healEvent = new List<DamageEvent>();
 
-    private void Awake()
+    private void Start()
     {
         timer = gameObject.AddComponent<StopWatch>();
 
-        HP = int.MaxValue;
-        MaxHP = int.MaxValue;
-        timer.LapTime = 1f;
         beat.Add(() => { CheckDead(); });
         timer.LapEvent = () => { beat.ForEach((live) => { live(); }); };
-
-        //error
-        if (HP <= 0)
-        {
-            Debug.Log("Default HP is 0.");
-        }
     }
 
     private bool CheckDead()
     {
         if (!(HP <= 0)) return false;
 
-        Debug.Log("lastword.");
         timer.Pause(true);
         dead.ForEach((lastword) => { lastword(); });
 
@@ -49,22 +40,22 @@ public class Life : MonoBehaviour
 
     public void AddLastword(Dead func)
     {
-        dead.Insert(0,func);
+        dead.Insert(0, func);
     }
 
     public void AddBeat(HeartBeat func)
     {
-        beat.Insert(0,func);
+        beat.Insert(0, func);
     }
 
     public void AddDamageFunc(DamageEvent func)
     {
-        damageEvent.Insert(0,func);
+        damageEvent.Insert(0, func);
     }
 
     public void AddHealFunc(DamageEvent func)
     {
-        healEvent.Insert(0,func);
+        healEvent.Insert(0, func);
     }
 
     public int Damage(int fouce)
@@ -87,7 +78,7 @@ public class Life : MonoBehaviour
     }
 
     //return true is error
-    public bool LifeSetup(int max_hp,int def_hp,uint def_beat)
+    public bool LifeSetup(int max_hp, int def_hp, uint def_beat)
     {
         BeatTime = def_beat;
         MaxHP = max_hp;
@@ -99,5 +90,11 @@ public class Life : MonoBehaviour
         }
 
         return false;
+    }
+
+    public void Freeze(bool freeze)
+    {
+        isFreeze = freeze;
+        timer.Pause(freeze);
     }
 }
