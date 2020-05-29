6883959a804d4173849d8959cd59d64781a19003
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-05-29 17:01:52 +0900

    敵とアーツの当たり判定

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/ArtsHit/ArtsHitMessage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtsHitMessage : MonoBehaviour
+{
+    public void HitMessage()// エネミーに攻撃が当たった時に呼ばれる
+    {
+        Debug.Log("エネミー「痛い…やめろぉ！」");
+    }
+}
