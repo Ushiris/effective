ffbb2164887450317ed4b571085b7c9b5d0ce1eb
konata016 <konata016@gmail.com>
2020-06-12 15:21:08 +0900

    更新

---
+++ Effective_project/Assets/KonaKameDan/Script/konata/PopUp/DisPopUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisPopUp : MonoBehaviour
+{
+    [SerializeField] float dis;
+    [SerializeField] GameObject target;
+    [SerializeField] GameObject my;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float f = Vector3.Distance(my.transform.position, target.transform.position);
+
+        if (f > dis)
+        {
+            if (!my.activeSelf) my.SetActive(true);
+        }
+        else
+        {
+            if (my.activeSelf) my.SetActive(false);
+        }
+    }
+}
