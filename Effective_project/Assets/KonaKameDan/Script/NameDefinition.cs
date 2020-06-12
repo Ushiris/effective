19febb8440ec028f1d5681bcf11fde82d0b45f00
konata016 <konata016@gmail.com>
2020-06-12 14:20:49 +0900

    エフェクトのenum決めた

---
+++ Effective_project/Assets/KonaKameDan/Script/NameDefinition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameDefinition : MonoBehaviour
+{
+    /// <summary>
+    /// Shot=射撃,Slash=斬撃,Barrier=防御,Trap=設置,Spread=拡散,Homing=追尾,Drain=吸収,Explosion=爆発,Slow=遅延,Fly=飛翔
+    /// </summary>
+    public enum EffectName
+    {
+        Shot, Slash, Barrier, Trap, Spread, Homing, Drain, Explosion, Slow, Fly
+    }
+}
