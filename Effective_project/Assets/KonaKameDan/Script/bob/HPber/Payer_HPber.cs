204b4981d0e57283b85f5ffacecdb9acd1832ef2
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-06-15 16:21:20 +0900

    HPバー

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/HPber/Payer_HPber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Payer_HPber : MonoBehaviour
+{
+    private float maxHP;
+    [SerializeField] private Status playerStatus;
+    public Slider berHP;
+    void Start()
+    {
+        GameObject playerStatusObject = GameObject.FindWithTag("Player");
+        playerStatus = playerStatusObject.GetComponent<Status>();
+        StatusUpdate();
+    }
+    public void StatusUpdate()
+    {
+        maxHP = playerStatus.status[Status.Name.HP];
+        berHP.maxValue = maxHP;// HPバーマックス
+        berHP.value = maxHP;// HP回復
+    }
+}
