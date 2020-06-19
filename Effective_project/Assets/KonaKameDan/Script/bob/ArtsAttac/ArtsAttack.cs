e790c2913a97efd2da6d948cc54d87282a6343c4
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-06-19 16:53:30 +0900

    artsの中身がないから放てないぞぉ！

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/ArtsAttac/ArtsAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtsAttack : MonoBehaviour
+{
+    [SerializeField] private Arts arts;
+    [SerializeField] private ArtsGenerator artsGenerator;
+    private GameObject artsAttack;
+    void Start()
+    {
+        GameObject gameManager = GameObject.Find("GameManager");
+        artsGenerator = gameManager.GetComponent<ArtsGenerator>();
+        arts = gameManager.GetComponent<Arts>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            /*
+            artsAttack = artsGenerator.GenerateArts(ArtsList.GetSelectArts.id);// 選択しているアーツのgameObjectを参照
+            artsAttack.GetComponent<Arts>().Fire();
+            */
+            Debug.Log("ここをクリックしてscript内のコメントを外さないとartsは出ません");
+        }
+    }
+}
