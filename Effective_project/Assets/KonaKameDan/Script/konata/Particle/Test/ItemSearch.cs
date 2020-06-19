7efda44cb81cf7da6b79bb61e0fa21efc07f5d24
konata016 <konata016@gmail.com>
2020-06-19 16:12:49 +0900

    マウスのポインターにアイコンを出すスクリプト作成

---
+++ Effective_project/Assets/KonaKameDan/Script/konata/Particle/Test/ItemSearch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSearch : MonoBehaviour
+{
+    [SerializeField] Vector3 fixPos;
+    public GameObject obj;
+    RectTransform rt;
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rt = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = RectTransformUtility.WorldToScreenPoint(Camera.main, obj.transform.position);
+
+        pos.x = Mathf.Clamp(pos.x, 0f, Screen.width);
+        pos.y = Mathf.Clamp(pos.y, 0f, Screen.height);
+
+        rt.position = pos;
+    }
+}
