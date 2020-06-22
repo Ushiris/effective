13db0c0791c50ebf7bfedbc8fa47e7252d9effce
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-06-22 15:04:49 +0900

    すたあとしいん

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/StartScene/StartButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    private bool startButton;
+
+    private void Start()
+    {
+        startButton = false;
+    }
+    private void Update()
+    {
+        if(startButton)
+            SceneManager.LoadScene("main");
+
+    }
+    public void OnClick()
+    {
+        startButton = true;
+    }
+}
