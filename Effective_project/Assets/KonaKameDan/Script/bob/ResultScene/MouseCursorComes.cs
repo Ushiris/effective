19c28ebf499de26a1869e398f9b35beab64828a1
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-06-22 18:17:26 +0900

    日本語とマウスカーソル

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/ResultScene/MouseCursorComes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursorComes : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = true;// マウスカーソル出現！
+    }
+}
