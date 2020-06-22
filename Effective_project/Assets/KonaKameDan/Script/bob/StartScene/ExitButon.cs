27eae63def11edef69b76e2f217440c4011c2bb0
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-06-22 15:21:33 +0900

    startScene

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/StartScene/ExitButon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButon : MonoBehaviour
+{
+    private bool exitButton;
+
+    private void Start()
+    {
+        exitButton = false;
+    }
+    private void Update()
+    {
+        if (exitButton)
+            UnityEditor.EditorApplication.isPlaying = false;
+
+    }
+    public void OnClick()
+    {
+        exitButton = true;
+    }
+}
