10bfbfa81b1838ff557ba42651298bdcfe0a0804
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-06-22 15:38:10 +0900

    リザルトから戻る

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/ResultScene/ReturnStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnStart : MonoBehaviour
+{
+    private bool returnStartScene;
+
+    private void Start()
+    {
+        returnStartScene = false;
+    }
+    private void Update()
+    {
+        if (returnStartScene)
+            SceneManager.LoadScene("Start");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            OnClick();
+
+    }
+    public void OnClick()
+    {
+        returnStartScene = true;
+    }
+}
