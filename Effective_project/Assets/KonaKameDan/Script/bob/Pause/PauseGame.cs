377d139bd19fce12ae41014dc913cb84aec2b5c3
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-06-24 20:26:40 +0900

    更新

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/Pause/PauseGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseUI;
+    [SerializeField] private GameObject pauseUIInstansce;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(pauseUIInstansce == null)
+            {
+                Pause();
+            }
+            else
+            {
+                NoPause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        pauseUIInstansce = GameObject.Instantiate(pauseUI) as GameObject;
+        Time.timeScale = 0.0f;// pause
+        Cursor.visible = true;// マウスカーソル出現！
+    }
+    public void NoPause()
+    {
+        Destroy(pauseUIInstansce);
+        Time.timeScale = 1.0f;
+        Cursor.visible = false;// マウスカーソ削除！
+    }
+}
