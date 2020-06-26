bdf613991b06955cea63805c0c91dd3541a2a5f8
konata016 <konata016@gmail.com>
2020-06-26 15:05:16 +0900

    速度の変更

--- Effective_project/Assets/KonaKameDan/Script/konata/Particle/Test/ItemSearch.cs
+++ Effective_project/Assets/KonaKameDan/Script/konata/Particle/Test/ItemSearch.cs
@@ -8,21 +8,36 @@ public class ItemSearch : MonoBehaviour
     [SerializeField] Vector3 fixPos;
     public GameObject obj;
     RectTransform rt;
-    Vector3 pos;
+    public Vector3 pos;
+
+    float maxX, minX;
+    float maxY, minY;
 
     // Start is called before the first frame update
     void Start()
     {
         rt = GetComponent<RectTransform>();
+
+        maxX = Screen.width;
+        minX = 0;
+        maxY = Screen.height;
+        minY = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        pos = RectTransformUtility.WorldToScreenPoint(Camera.main, obj.transform.position);
+        Vector3 objPos = obj.transform.position;
+        pos = RectTransformUtility.WorldToScreenPoint(Camera.main, new Vector3(objPos.x, objPos.z, objPos.y));
 
-        pos.x = Mathf.Clamp(pos.x, 0f, Screen.width);
-        pos.y = Mathf.Clamp(pos.y, 0f, Screen.height);
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        if (pos.x == 0)
+        {
+            rt.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+        }
+
 
         rt.position = pos;
     }
