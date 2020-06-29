a6f8597e201c57f48acbb2ea785efd7114672014
ushiris_desktop <ushiriscco@gmail.com>
2020-06-29 17:08:07 +0900

    update fade

--- Effective_project/Assets/KonaKameDan/Script/ushiris/FadeOut.cs
+++ Effective_project/Assets/KonaKameDan/Script/ushiris/FadeOut.cs
@@ -8,8 +8,22 @@ public class FadeOut : MonoBehaviour
 {
     Image Fade;
 
+    public float fadeTime = 2.0f;
+    public float fadeStartTime = 0f;
+    public Color endColor = Color.black;
+
+    float time;
+    
     private void Start()
     {
         Fade = GetComponent<Image>();
+        Fade.color = Color.clear;
+        time = 0;
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        Fade.color = new Color(Fade.color.r, Fade.color.g, Fade.color.b, time / fadeTime);
     }
 }
