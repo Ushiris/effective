c5abcfe685752731ccf8f73380666585938a5547
ushiris_desktop <ushiriscco@gmail.com>
2020-07-06 16:27:22 +0900

    add slow

--- Effective_project/Assets/KonaKameDan/Script/ushiris/FadeOut.cs
+++ Effective_project/Assets/KonaKameDan/Script/ushiris/FadeOut.cs
@@ -8,7 +8,7 @@ public class FadeOut : MonoBehaviour
 {
     Image Fade;
 
-    public float fadeTime = 2.0f;
+    public float fadeTime = 4.0f;
     public float fadeStartTime = 0f;
     public Color endColor = Color.black;
 
@@ -38,6 +38,8 @@ public class FadeOut : MonoBehaviour
     private void Update()
     {
         time += Time.deltaTime;
-        Fade.color = new Color(Fade.color.r, Fade.color.g, Fade.color.b, time / fadeTime);
+        if (time <= fadeStartTime) return;
+
+        Fade.color = new Color(Fade.color.r, Fade.color.g, Fade.color.b, time / fadeTime-fadeStartTime);
     }
 }
