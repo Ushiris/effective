c50cb0b52926c98927b8bf45fda9c0ddff94071c
konata016 <konata016@gmail.com>
2020-07-11 00:06:13 +0900

    Shieldシェーダー作成

---
+++ Effective_project/Assets/KonaKameDan/Script/konata/Arts/Test/Id25_UnbreakableShield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Id25_UnbreakableShield : MonoBehaviour
+{
+    [SerializeField] GameObject shieldObj;
+    [SerializeField] float timeOver = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
