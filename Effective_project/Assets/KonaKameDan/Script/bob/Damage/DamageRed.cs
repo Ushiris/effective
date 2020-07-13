2aa9e82b5006095ae217cc7198db1b19cecf6b06
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-07-13 16:56:00 +0900

    ダメージヒットした時の画面赤くなるやつ
    Scenes→Damage→DamageRedで確認可能

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/Damage/DamageRed.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageRed : MonoBehaviour
+{
+    private Image damageImg;
+    public static bool damageHidRed { private get; set; }
+    void Start()
+    {
+        damageImg = GetComponent<Image>();
+        damageImg.color = Color.clear;
+        damageHidRed = false;
+    }
+
+    void Update()
+    {
+        if(damageHidRed)
+        {
+            this.damageImg.color = new Color(0.5f, 0.0f, 0.0f, 0.5f);
+            damageHidRed = false;
+        }
+        else
+        {
+            this.damageImg.color = Color.Lerp(this.damageImg.color, Color.clear, Time.deltaTime);
+        }
+    }
+
+    public void ButtonOn()
+    {
+        damageHidRed = true;
+    }
+}
