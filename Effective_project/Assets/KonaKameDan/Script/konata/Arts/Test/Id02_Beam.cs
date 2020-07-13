6fe327e48467a9c248094b22f31ca4bf02a6ef53
konata016 <konata016@gmail.com>
2020-07-14 02:29:21 +0900

    ビームと回避の追加(仮)

---
+++ Effective_project/Assets/KonaKameDan/Script/konata/Arts/Test/Id02_Beam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Id02_Beam : MonoBehaviour
+{
+    [SerializeField] GameObject beamParticleObj;
+    [SerializeField] float force = 100f;
+    [SerializeField] float lostTime = 3f;
+
+    StopWatch timer;
+    ArtsStatus artsStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject beam = Instantiate(beamParticleObj, transform);
+        Arts_Process.SetParticleDamageProcess(beam);
+        Arts_Process.RbMomentMove(beam, force);
+
+        timer = Arts_Process.TimeAction(gameObject, 10);
+        timer.LapEvent = () => { Destroy(gameObject); };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Effective_project/Assets/KonaKameDan/Script/konata/Arts/Test/Id29_Escape.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Id29_Escape : MonoBehaviour
+{
+    [SerializeField] float force = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 vector = new Vector3(Input.GetAxis("Vertical"), 0f, Input.GetAxis("Horizontal"));
+        var obj = PlayerManager.GetManager.GetPlObj;
+        Arts_Process.RbMomentMove(obj, -force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject);
+    }
+}
