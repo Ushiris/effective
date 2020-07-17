2a54b391edfb30808dc5734a02bb684670315522
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-07-17 16:29:48 +0900

    ふわふわ飛びます

--- Effective_project/Assets/KonaKameDan/Script/bob/Enemy/EnemyMove_fry.cs
+++ Effective_project/Assets/KonaKameDan/Script/bob/Enemy/EnemyMove_fry.cs
@@ -5,33 +5,32 @@ using UnityEngine;
 public class EnemyMove_fry : MonoBehaviour
 {
     private Transform player;
-    private Transform oldEnemyPos;
     private Rigidbody rb;
     private float speed = 2;
     private float fryPosUp = 5;
     private float fryPosDown = 3;
-    private bool positioning;
 
     void Start()
     {
-        player = PlayerManager.GetManager.GetPlObj.transform;
+        Debug.Log("メインシーン実装時にはここをクリックしてコメントをとっておいて！");
+        //player = PlayerManager.GetManager.GetPlObj.transform;
         rb = GetComponent<Rigidbody>();
-        positioning = true;
         transform.position = new Vector3(transform.position.x, Random.Range(fryPosDown, fryPosUp), transform.position.z);
     }
 
     void Update()
     {
-        transform.LookAt(player);
+        Debug.Log("メインシーン実装時にはここをクリックしてコメントをとっておいて！");
+        //transform.LookAt(player);
 
-        if (Vector3.Distance(player.position, transform.position) < 3)// プレイヤーが範囲内にいるとき
-        {
-            rb.velocity = Vector3.zero;
-        }
-        else// プレイヤーが範囲内にいないとき
-        {
-            rb.velocity = transform.forward * speed;
-        }
+        //if (Vector3.Distance(player.position, transform.position) < 3)// プレイヤーが範囲内にいるとき
+        //{
+        //    rb.velocity = Vector3.zero;
+        //}
+        //else// プレイヤーが範囲内にいないとき
+        //{
+        //    rb.velocity = transform.forward * speed;
+        //}
 
         if (this.transform.position.y > fryPosUp)
         {
