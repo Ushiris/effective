fcf6968a64eeb5499c1f6535e297c8cc7589144d
ushiris_desktop <ushiriscco@gmail.com>
2020-07-17 16:42:06 +0900

    add input manager

---
+++ Effective_project/Assets/KonaKameDan/Script/ushiris/PlayerInput.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    enum InputName:byte
+    {
+        W,
+        A,
+        S,
+        D,
+        E,
+        Q,
+        Esc,
+        PushMouseLeft,
+        ReleseMouceLeft,
+        One,
+        Twe,
+        Three,
+        INPUT_COUNT
+    }
+
+    List<KeyCode> useKey = new List<KeyCode>{
+        KeyCode.W,
+        KeyCode.A,
+        KeyCode.S,
+        KeyCode.D,
+        KeyCode.E,
+        KeyCode.Q,
+        KeyCode.Escape,
+        KeyCode.Mouse0,
+        KeyCode.Alpha1,
+        KeyCode.Alpha2,
+        KeyCode.Alpha3
+    };
+
+    public delegate bool PlayerAction();
+
+    public List<bool> inputs = new List<bool>();
+
+    bool isPause { get { return Time.timeScale < 0.1; } }
+
+
+
+    private void Update()
+    {
+        
+    }
+}
