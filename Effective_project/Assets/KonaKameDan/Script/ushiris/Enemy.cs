915cd5e664c47a27b8baa59b89f6ca81879e88e7
ushiris_desktop <ushiriscco@gmail.com>
2020-07-24 15:43:38 +0900

    add effect drop

--- Effective_project/Assets/KonaKameDan/Script/ushiris/Enemy.cs
+++ Effective_project/Assets/KonaKameDan/Script/ushiris/Enemy.cs
@@ -12,10 +12,14 @@ public class Enemy : MonoBehaviour
     static Vector3 hp_small = new Vector3(1, 1, 1);
     static Vector3 big = new Vector3(3, 2, 1);
 
+    MyEffectCount bag;
+
     private void Start()
     {
         life = gameObject.AddComponent<Life>();
         slider = GetComponentInChildren<Slider>();
+        if (GetComponent<MyEffectCount>() == null) bag = gameObject.AddComponent<MyEffectCount>();
+        else bag = gameObject.GetComponent<MyEffectCount>();
 
         //sliderの初期化
         slider.minValue = 0;
@@ -32,6 +36,7 @@ public class Enemy : MonoBehaviour
 
         //Lifeの初期化
         if (life.LifeSetup(1)) Debug.Log("Error init HP");
+        life.AddLastword(DropEffect);
         life.AddLastword(Dead);
         life.AddDamageFunc(Damage);
         life.AddHealFunc(Heal);
@@ -75,6 +80,20 @@ public class Enemy : MonoBehaviour
         bullet_i.GetComponent<Rigidbody>().velocity = Vector3.zero;
         
         bullet_i.GetComponent<Rigidbody>().AddForce(velocity * bullet_i.GetComponent<Rigidbody>().mass, ForceMode.Impulse);
+    }
+
+    void DropEffect()
+    {
+        var EffectList = new List<NameDefinition.EffectName>();
+        foreach (var item in bag.effectCount)
+        {
+            for (int i = 0; i < item.Value; i++) EffectList.Add(item.Key);
+        }
+        var rand = Random.Range(0, EffectList.Count);
+        var name = EffectList[rand];
+
+        var effect = Instantiate(Resources.Load("EffectObj/[" + name.ToString() + "]EffectObject")) as GameObject;
+        effect.transform.position = gameObject.transform.position;
     }
 
     void Dead()
