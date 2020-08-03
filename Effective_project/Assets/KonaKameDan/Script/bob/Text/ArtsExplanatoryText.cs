3777545bec56759b0cfc841154213cfb0cd4d084
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-08-03 15:19:55 +0900

    説明文かけるところ作ったお

---
+++ Effective_project/Assets/KonaKameDan/Script/bob/Text/ArtsExplanatoryText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ArtsExplanatoryText : MonoBehaviour
+{
+    [SerializeField] private ArtsTextDictionary artsTextDictionary;
+    [SerializeField] private string artsName;
+    private string artsNameTmp;
+    public TextMeshProUGUI artsText;
+    private void Start()
+    {
+        artsTextDictionary = GetComponent<ArtsTextDictionary>();
+    }
+    void Update()
+    {
+        artsName = MyArtsDeck.GetSelectArtsDeck.id;
+        if (artsName != artsNameTmp)
+        {
+            artsText.text = artsTextDictionary.ArtsText[artsName];// 説明文入力
+            artsNameTmp = artsName;
+        }
+    }
+}
