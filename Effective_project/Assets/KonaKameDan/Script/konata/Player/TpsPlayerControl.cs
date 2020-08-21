8d029c8619736e14acfd23c824ae8a0c4627bc2c
ushiris_desktop <ushiriscco@gmail.com>
2020-08-21 14:48:58 +0900

    fix camera

--- Effective_project/Assets/KonaKameDan/Script/konata/Player/TpsPlayerControl.cs
+++ Effective_project/Assets/KonaKameDan/Script/konata/Player/TpsPlayerControl.cs
@@ -7,16 +7,8 @@ using UnityEngine;
 /// </summary>
 public class TpsPlayerControl : MonoBehaviour
 {
-
     [SerializeField] GameObject cameraPivot;
     [SerializeField] GameObject pl;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -25,6 +17,5 @@ public class TpsPlayerControl : MonoBehaviour
         float Y_Rotation = Input.GetAxis("Mouse Y") * PlayerManager.GetManager.mouseSensitivity;
         pl.transform.Rotate(0, X_Rotation, 0);
         cameraPivot.transform.Rotate(-Y_Rotation, 0, 0);
-
     }
 }
