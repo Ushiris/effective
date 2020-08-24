e4edfa03c3fd181580b208f534ad5378d5e6bef4
ushiris_desktop <ushiriscco@gmail.com>
2020-08-24 18:47:46 +0900

    fix pre

--- Effective_project/Assets/KonaKameDan/Script/konata/Player/TpsPlayerControl.cs
+++ Effective_project/Assets/KonaKameDan/Script/konata/Player/TpsPlayerControl.cs
@@ -7,24 +7,46 @@ using UnityEngine;
 /// </summary>
 public class TpsPlayerControl : MonoBehaviour
 {
-
     [SerializeField] GameObject cameraPivot;
     [SerializeField] GameObject pl;
+    [SerializeField] GameObject cameraObj;
+    [SerializeField] LayerMask wallLayers;
+    [SerializeField] float distance = 1.5f;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector3 targetPosition, desiredPosition, wallHitPosition = Vector3.zero;
+    RaycastHit wallHit;
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float X_Rotation = Input.GetAxis("Mouse X") * PlayerManager.GetManager.mouseSensitivity;
         float Y_Rotation = Input.GetAxis("Mouse Y") * PlayerManager.GetManager.mouseSensitivity;
         pl.transform.Rotate(0, X_Rotation, 0);
         cameraPivot.transform.Rotate(-Y_Rotation, 0, 0);
 
+        var dist = Vector3.Distance(cameraObj.transform.position, pl.transform.position);
+        if (dist != distance)
+        {
+            var diff = cameraObj.transform.position - pl.transform.position;
+            cameraObj.transform.position = diff * (distance / dist);
+        }
+
+        desiredPosition = -cameraObj.transform.position;
+
+        cameraObj.transform.position = WallCheck() ? wallHitPosition : desiredPosition;
+    }
+
+    protected bool WallCheck()
+    {
+        targetPosition = pl.transform.position;
+        if (Physics.Raycast(targetPosition, desiredPosition - targetPosition, out wallHit, Vector3.Distance(targetPosition, desiredPosition), wallLayers, QueryTriggerInteraction.Ignore))
+        {
+            wallHitPosition = wallHit.point;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
