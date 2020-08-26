588467ecac1df6735c6b9199a38a1a34d330438c
konata016 <konata016@gmail.com>
2020-08-27 00:16:04 +0900

    Arts追加

---
+++ Effective_project/Assets/KonaKameDan/Script/konata/Arts/Test/Id057_Managarmr.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Id057_Managarmr : MonoBehaviour
+{
+    [SerializeField] GameObject managarmrParticleObj;
+    [SerializeField] float force = 30f;
+
+    GameObject managarmrParticle;
+    GameObject target;
+
+    new ParticleSystem particleSystem;
+    ArtsStatus artsStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        artsStatus = GetComponent<ArtsStatus>();
+
+        managarmrParticle = Instantiate(managarmrParticleObj, transform);
+        particleSystem = managarmrParticle.GetComponent<ParticleSystem>();
+
+        //敵のポジションを持ってくる
+        target = Arts_Process.GetNearTarget(artsStatus);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ホーミング処理
+        if (target != null && managarmrParticle != null)
+        {
+            Arts_Process.HomingParticle(particleSystem, target, force);
+        }
+
+        //オブジェクトを消す
+
+        if (managarmrParticle != null)
+        {
+            if (managarmrParticle.transform.childCount == 0) Destroy(gameObject);
+        }
+    }
+}
---
+++ Effective_project/Assets/KonaKameDan/Script/konata/Arts/Test/Id079_Amaterasu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Id079_Amaterasu : MonoBehaviour
+{
+    [SerializeField] GameObject satelliteCannonParticleObj;
+    [SerializeField] GameObject satelliteCannonStartParticleObj;
+    [SerializeField] Vector3 instantPos = new Vector3(0, 0, 5);
+
+    bool isSatelliteCannonInstant;
+    GameObject satelliteCannonStart;
+
+    ArtsStatus artsStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //artsStatus = GetComponent<ArtsStatus>();
+
+        //位置の初期設定
+        transform.parent = null;
+        var rot = transform.rotation;
+        var rotV3 = new Vector3(0, 1, 0) * rot.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(rotV3);
+        transform.localPosition += instantPos;
+
+        //初手の演出生成
+        satelliteCannonStart = Instantiate(satelliteCannonStartParticleObj, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (satelliteCannonStart == null && !isSatelliteCannonInstant)
+        {
+            isSatelliteCannonInstant = true;
+
+            //サテライトキャノンの生成
+            var obj= Instantiate(satelliteCannonParticleObj, transform);
+            obj.transform.localPosition = new Vector3(0, 10, 0);
+        }
+
+        //オブジェクトを消す
+        if (isSatelliteCannonInstant)
+        {
+            if (transform.childCount == 0) Destroy(gameObject);
+        }
+    }
+}
