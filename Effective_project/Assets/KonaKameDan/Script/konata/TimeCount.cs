cc505eff601fda11c41b1780edb02381d4c63e05
konata016 <konata016@gmail.com>
2020-09-07 19:10:55 +0900

    プレイヤーのジャンプ追加

---
+++ Effective_project/Assets/KonaKameDan/Script/konata/TimeCount.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeCount : MonoBehaviour
+{
+    public float setTime;
+    public bool onAlarm { get; private set; }
+    public bool isPlay { get; private set; }
+    float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlay)
+        {
+            if (time < setTime)
+            {
+                time += Time.deltaTime;
+            }
+            else
+            {
+                isPlay = false;
+                onAlarm = true;
+                time = 0;
+            }
+        }
+    }
+
+    //スタートさせる
+    public void ResetAndStart()
+    {
+        onAlarm = false;
+        isPlay = true;
+    }
+}
