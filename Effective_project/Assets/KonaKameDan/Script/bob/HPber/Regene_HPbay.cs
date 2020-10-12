52b703aeb77c1d11c834fc6e37a5f17b7e7c417c
BOB2o <g1727044@st.t-kougei.ac.jp>
2020-10-12 15:24:02 +0900

    HPBar途中経過

--- Effective_project/Assets/KonaKameDan/Script/bob/HPber/Regene_HPbay.cs
+++ Effective_project/Assets/KonaKameDan/Script/bob/HPber/Regene_HPbay.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Regene_HPbay : MonoBehaviour
 {
+    Image image;
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
     void Update()
     {
+        if (PlayerManager.GetManager.IsRegene())
+        {
+            image.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        }
+        else
+        {
+            image.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        }
     }
 }
