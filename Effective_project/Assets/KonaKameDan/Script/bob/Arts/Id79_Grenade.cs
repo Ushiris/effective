b70525cfef218273c9db370006b46bc62b256d7c
konata016 <konata016@gmail.com>
2020-10-26 17:03:47 +0900

    グレネードDamage追加

--- Effective_project/Assets/KonaKameDan/Script/bob/Arts/Id79_Grenade.cs
+++ Effective_project/Assets/KonaKameDan/Script/bob/Arts/Id79_Grenade.cs
@@ -12,6 +12,18 @@ public class Id79_Grenade : MonoBehaviour
     [SerializeField] float trajectorySpace = 0.1f;
     [SerializeField] float lostTime = 1;
     [SerializeField] float slidingThroughLostTime = 5;
+    [SerializeField] float defaultDamage = 1.2f;
+
+    [Header("爆発のスタック数に応じてたされる数")]
+    [SerializeField] float plusDamage = 0.02f;
+
+    [Header("飛翔のスタック数に応じてたされる数")]
+    [SerializeField] float addForth = 0.5f;
+
+    //エフェクトの所持数用
+    int explosionCount = 0;
+    int flyCount = 0;
+    float damage;
 
     Vector3 pos;
     GameObject grenade;
@@ -27,6 +39,10 @@ public class Id79_Grenade : MonoBehaviour
     {
         artsStatus = GetComponent<ArtsStatus>();
 
+        //エフェクトの所持数を代入
+        explosionCount = Arts_Process.GetEffectCount(artsStatus, NameDefinition.EffectName.Explosion);
+        flyCount = Arts_Process.GetEffectCount(artsStatus, NameDefinition.EffectName.Fly);
+
         for (int i = 0; i < trajectoryCount; i++)
         {
             miracles.Add(Instantiate(trajectoryObj, transform));
@@ -34,7 +50,6 @@ public class Id79_Grenade : MonoBehaviour
 
         //軌跡を生成
         miracles = Arts_Process.Trajectory(miracles, trajectorySpace, v0);
-        
     }
 
     // Update is called once per frame
@@ -51,10 +66,11 @@ public class Id79_Grenade : MonoBehaviour
             Rigidbody jumpCubeRb = grenade.GetComponent<Rigidbody>();
             jumpCubeRb.AddRelativeFor​​ce(v0, ForceMode.VelocityChange);
 
+            Damage(grenade);
+
             //爆発するエフェクトのセット
             particleHitPlay =
                 Arts_Process.SetParticleHitPlay(grenade, grenadeObj, transform, artsStatus, lostTime, ParticleHitPlayExplosion.Mode.My, true);
-
             isStart = false;
 
             for (int i = 0; i < trajectoryCount; i++)
@@ -86,5 +102,17 @@ public class Id79_Grenade : MonoBehaviour
             var explosion = Instantiate(grenadeObj, pos, Quaternion.identity);
             Destroy(gameObject, 2);
         }
+    }
+
+    void Damage(GameObject obj)
+    {
+        //ダメージ
+        var objDamage = Arts_Process.SetParticleDamageProcess(obj);
+
+        //ダメージの計算
+        damage = defaultDamage + (plusDamage * (float)explosionCount);
+
+        //ダメージ処理
+        Arts_Process.Damage(objDamage, artsStatus, damage, true);
     }
 }
