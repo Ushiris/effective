4ea0e3edb3026ad856aaf4d76cd66b8d6bf93499
konata016 <konata016@gmail.com>
2020-11-02 00:15:48 +0900

    Artsを出す者によって色を変える

--- Effective_project/Assets/KonaKameDan/Shader/SatelliteBeam/SatelliteBeamMaterial.cs
+++ Effective_project/Assets/KonaKameDan/Shader/SatelliteBeam/SatelliteBeamMaterial.cs
@@ -16,8 +16,12 @@ public class SatelliteBeamMaterial : MonoBehaviour
     [SerializeField] Material shader;
     [SerializeField] Material coreMaterial;
 
-    readonly float shaderSpeed = 1;
-    readonly float coreMaterialSpeed = 1.5f;
+    static readonly float shaderSpeed = 1;
+    static readonly float coreMaterialSpeed = 1.5f;
+    static readonly string kCoreMaterialColorName = "_BaseColor";
+    static readonly string kCoreMaterialEnissionColorName = "_EmissiveColor";
+    static readonly Color32 kBooleColorCode = new Color32(18, 0, 255, 0);
+    static readonly Color32 kRedColorCode = new Color32(255, 44, 0, 0);
 
     float timer;
     float coreMaterialTimer = 1;
@@ -25,15 +29,30 @@ public class SatelliteBeamMaterial : MonoBehaviour
 
     private void Start()
     {
+        ArtsStatus artsStatus = GetComponentInParent<ArtsStatus>();
+
         //マテリアルの複製
         shader = shaderObj.GetComponent<Renderer>().material = new Material(shader);
-        coreMaterial = coreMaterialObj.GetComponent<Renderer>().material= new Material(coreMaterial);
+        coreMaterial = coreMaterialObj.GetComponent<Renderer>().material = new Material(coreMaterial);
 
         //初期化
         shader.SetFloat("Vector1_5D2414AE", 0);
         var color = coreMaterial.color;
         color.a = coreMaterialTimer;
         coreMaterial.color = color;
+
+        //Artsを出す者によって色を変える
+        switch (artsStatus.type)
+        {
+            case ArtsStatus.ParticleType.Player:
+                coreMaterial.SetColor(kCoreMaterialEnissionColorName, kBooleColorCode);
+                break;
+            case ArtsStatus.ParticleType.Enemy:
+                coreMaterial.SetColor(kCoreMaterialEnissionColorName, kRedColorCode);
+                break;
+            case ArtsStatus.ParticleType.Unknown:break;
+            default:break;
+        }
     }
 
     // Update is called once per frame
@@ -67,9 +86,9 @@ public class SatelliteBeamMaterial : MonoBehaviour
     void CoreMaterial(float speed)
     {
         coreMaterialTimer -= speed * Time.deltaTime;
-        var color = coreMaterial.GetColor("_BaseColor");
+        var color = coreMaterial.GetColor(kCoreMaterialColorName);
         color.a = coreMaterialTimer;
         color.a = Mathf.Clamp(color.a, 0, 100);
-        coreMaterial.SetColor("_BaseColor", color);
+        coreMaterial.SetColor(kCoreMaterialColorName, color);
     }
 }
