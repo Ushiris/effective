7b599811760aa5b7acc7f7e7d836d1ba732fd558
ushiris_desktop <ushiriscco@gmail.com>
2020-11-13 13:59:40 +0900

    fix enemy

---
+++ NewEffective/Assets/KonaKameDan/Script/ushiris/Enemy/IEnemySetting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum ExAItype
+{
+    Static,
+    Deffender,
+    Sniper
+}
+public enum FindAItype
+{
+    Soldier,
+    Commander
+}
+public enum StayAItype
+{
+    Ambush,
+    Ninja,
+    Return,
+    follower
+}
+
+public interface IEnemySetting
+{
+    void SetAI(ExAItype type);
+
+    void SetAI(FindAItype type);
+
+    void SetAI(StayAItype type);
+}
