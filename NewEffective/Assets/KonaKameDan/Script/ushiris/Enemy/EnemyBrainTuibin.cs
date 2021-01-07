7524dbbe00e1e41d904894d40b051601ad7a110b
ushiris_desktop <ushiriscco@gmail.com>
2021-01-07 16:09:17 +0900

    fix enemy

--- NewEffective/Assets/KonaKameDan/Script/ushiris/Enemy/EnemyBrainTuibin.cs
+++ NewEffective/Assets/KonaKameDan/Script/ushiris/Enemy/EnemyBrainTuibin.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class EnemyBrainTuibin : EnemyBrainBase
 {
+    PlayerManager playerManager;
 
+    private new void Start()
+    {
+        base.Start();
+
+        playerManager = PlayerManager.GetManager;
+
+        AIset(FindAItype.Soldier);
+        AIset(StayAItype.Ambush);
+    }
+
+    void StayMove()
+    {
+        navMesh.SetDestination(playerManager.tracePoint);
+    }
 }
--- NewEffective/Assets/KonaKameDan/Script/ushiris/Enemy/KameMotion.cs
+++ NewEffective/Assets/KonaKameDan/Script/ushiris/Enemy/KameMotion.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class KameMotion : MonoBehaviour
 {
     [SerializeField] Animator animator;
     bool isMove = false;
+    NavMeshAgent agent;
 
+    private void Start()
+    {
+        agent = gameObject.GetComponentInParent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        isMove = !agent.isStopped;
+    }
 }
