7724cfbd3eced51bfef2d691f4438f14cb08685d
ushiris_desktop <ushiriscco@gmail.com>
2021-01-07 14:52:06 +0900

    add kame

---
+++ NewEffective/Assets/KonaKameDan/Script/ushiris/Enemy/KameMotion.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameMotion : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    bool isMove = false;
+
+}
