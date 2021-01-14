79a8261c77cd8a6d8fd9d00b68f080ccdb8fa81c
ushiris_desktop <ushiriscco@gmail.com>
2021-01-14 10:57:02 +0900

    add kakusan moves

---
+++ NewEffective/Assets/KonaKameDan/Script/ushiris/Enemy/EnemyBrainKakusan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBrainKakusan : EnemyBrainBase
+{
+    [SerializeField] SkinnedMeshRenderer changePoint;
+    [SerializeField] int material_index;
+
+    private new void Start()
+    {
+        mainMaterial = changePoint.materials[material_index];
+        ApplyChangeColor = () =>
+        {
+            var temp = changePoint.materials;
+            temp[material_index] = mainMaterial;
+            changePoint.materials = temp;
+        };
+        base.Start();
+
+        AIset(FindAItype.Soldier);
+        AIset(StayAItype.Ambush);
+    }
+}
