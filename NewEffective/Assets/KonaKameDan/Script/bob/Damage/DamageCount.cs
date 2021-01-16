faecbb4e2ce143063e813a81cbcf581adcc4658c
BOB2o <g1727044@st.t-kougei.ac.jp>
2021-01-16 20:21:12 +0900

    ダメージUIの修正

--- NewEffective/Assets/KonaKameDan/Script/bob/Damage/DamageCount.cs
+++ NewEffective/Assets/KonaKameDan/Script/bob/Damage/DamageCount.cs
@@ -17,7 +17,31 @@ public class DamageCount : MonoBehaviour
 
 	public static void Damage(GameObject col)
 	{
+		Vector3 textPos = Camera.main.transform.up * 1.0f - Camera.main.transform.forward * 3.0f;
+		// ダメージ表記を出すオブジェクトの高さに合わせて、高さを決める
+		if (col == GameObject.Find("TreasureBox"))// クリスタル
+		{
+			textPos = Camera.main.transform.up * 1.5f - Camera.main.transform.forward * 1.0f;
+			Debug.Log("クリスタル");
+		}
+		else if (col == GameObject.Find("enemyShot"))// 射撃の敵
+		{
+			textPos = Camera.main.transform.up * 1.0f - Camera.main.transform.forward * 1.0f;
+			Debug.Log("射撃");
+		}
+		else if (col == GameObject.Find("enemyFry"))// 飛翔の敵
+		{
+			textPos = Camera.main.transform.up * 0.5f - Camera.main.transform.forward * 1.0f;
+			Debug.Log("飛翔");
+		}
+		else if (col == GameObject.Find("enemyBomb"))// 爆発の敵
+		{
+			textPos = Camera.main.transform.up * 4.0f - Camera.main.transform.forward * 3.0f;
+			Debug.Log("爆発");
+		}
 		//　DamageUIをインスタンス化。登場位置は接触したコライダの中心からカメラの方向に少し寄せた位置
-		var obj = Instantiate(damageUI, col.transform.position - Camera.main.transform.forward * 1.0f, Quaternion.identity);
+		var obj = Instantiate(damageUI, col.transform.position + textPos, Quaternion.identity);
+		obj.transform.SetParent(col.transform);// 親（ダメージを食らったもの）の子オブジェクトにすることで、ダメージ表記をついてこさせるようにする
+		Debug.Log("今当たったものの名前 : " + col + " textPos : " + textPos);
 	}
 }
