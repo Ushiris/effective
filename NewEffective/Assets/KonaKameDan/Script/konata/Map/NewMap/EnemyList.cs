cf856e3df31474767a74b43ca04dd636507823f8
konata016 <konata016@gmail.com>
2021-01-29 17:26:19 +0900

    bossの種類をランダムで決める

---
+++ NewEffective/Assets/KonaKameDan/Script/konata/Map/NewMap/EnemyList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyList : MonoBehaviour
+{
+    [SerializeField] PrefabDictionary prefabs;
+
+    public GameObject GetGameObject(NameDefinition.EffectName name)
+    {
+        return prefabs.GetTable()[name];
+    } 
+
+    [System.Serializable]
+    public class PrefabDictionary : Serialize.TableBase<NameDefinition.EffectName, GameObject, Name2Prefab> { }
+
+    [System.Serializable]
+    public class Name2Prefab : Serialize.KeyAndValue<NameDefinition.EffectName, GameObject>
+    {
+        public Name2Prefab(NameDefinition.EffectName key, GameObject value) : base(key, value) { }
+    }
+}
