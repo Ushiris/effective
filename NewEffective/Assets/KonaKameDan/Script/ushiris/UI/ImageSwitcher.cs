ce8b145689ce2907178241fd96bbb73f44a4b6b4
ushiris_desktop <ushiriscco@gmail.com>
2021-02-18 07:56:16 +0900

    add tutorial

---
+++ NewEffective/Assets/KonaKameDan/Script/ushiris/UI/ImageSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSwitcher : MonoBehaviour
+{
+    [SerializeField] Image on, off;
+
+    private void Awake()
+    {
+        if (on == null) on = transform.Find("on").gameObject.GetComponent<Image>();
+        if (off == null) off = transform.Find("off").gameObject.GetComponent<Image>();
+
+        Switch(false);
+    }
+
+    public void Switch(bool IsOn)
+    {
+        Visible(on, IsOn);
+        Visible(off, !IsOn);
+    }
+
+    void Visible(Image img, bool state)
+    {
+        var color = img.color;
+        color.a = state ? 1 : 0;
+        img.color = color;
+    }
+}
---
+++ NewEffective/Assets/KonaKameDan/Script/ushiris/UI/KeyGuideControl.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyGuideControl : MonoBehaviour
+{
+    [SerializeField] ImageSwitcher w, a, s, d, space, wheel, left;
+
+
+}
